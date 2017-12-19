d555119ba42e8290b7836391ddeec08ae27bcc1e
Ilan Dubois <ilan.dubois@epita.fr>
2017-12-19 18:19:58 +0100

    Fix logger in cpu_miner and difficulty check

--- common/cpu_miner.py
+++ common/cpu_miner.py
@@ -21,13 +21,13 @@ class cpu_miner:
     def compute_hashes(self):
         i = 0
         while self.not_interrupted:
-            self.logging.info('Starting batch', i)
+            self.logging.info('Starting batch ' + str(i))
             self.block.updateTime()
             with Pool(None) as p:
                 val = p.map(try_pad, [(deepcopy(self.block), x) for x in range(i * self.batch, (i + 1) * self.batch)])
 
                 for j, v in enumerate(val):
-                    if v < self.block:
+                    if v < self.block.difficulty:
                         return (i * self.batch + j, self.not_interrupted)
 
                 i = i+1 if (i+1) * self.batch < 2**32 else 0
@@ -38,5 +38,5 @@ def try_pad(arg):
     block, pad = arg
     block.pad = pad
     m = hashlib.sha256(block.getPack()).digest()
-    value = int.from_bytes(a, byteorder='little', signed=False)
+    value = int.from_bytes(m, byteorder='little', signed=False)
     return value
